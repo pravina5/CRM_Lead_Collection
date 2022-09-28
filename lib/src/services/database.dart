190bfc164612b79882b11c1cc26ef52335d7fa42
pravina5 <48323089+pravina5@users.noreply.github.com>
2022-09-28 11:32:46 +0530

    Cleaned database.dart

--- lib/src/services/database.dart
+++ lib/src/services/database.dart
@@ -22,31 +22,5 @@ class DatabaseMethods {
     });
 
   }
-  searchByName(String searchField) async {
-    return await FirebaseFirestore.instance.collection("users").where('name', isEqualTo: searchField).get();
-  }
-
-  searchByInterest(String searchinterest) {
-    return FirebaseFirestore.instance.collection("users").where('interest', isEqualTo: searchinterest).get();
-  }
-
-  addChatRoom(chatRoom, chatRoomId) {
-    FirebaseFirestore.instance.collection("ChatRoom").doc(chatRoomId).set(chatRoom).catchError((e) {
-      print(e);
-    });
-  }
-
-  getChats(String chatRoomId) async {
-    return FirebaseFirestore.instance.collection("ChatRoom").doc(chatRoomId).collection("chats").orderBy('time').snapshots();
-  }
-
-  // Future<void> addMessage(String chatRoomId, chatMessageData) {
-  //   FirebaseFirestore.instance.collection("ChatRoom").doc(chatRoomId).collection("chats").add(chatMessageData).catchError((e) {
-  //     print(e.toString());
-  //   });
-  // }
-
-  getUserChats(String itIsMyName) async {
-    return await FirebaseFirestore.instance.collection("ChatRoom").where('user', arrayContains: itIsMyName).snapshots();
-  }
+  
 }
